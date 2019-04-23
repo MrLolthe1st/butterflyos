6f1061f8223974c51e82b5dbc540a48ab349d6db
mrlolthe1st <newcrafter@mail.ru>
2019-04-23 03:18:49 +0300

    no message

--- configure/configureMinGW.bat
+++
@@ -1,6 +0,0 @@
-set mingw=C:\Program Files (x86)\CodeBlocks\MinGW
-set mversion=5.1.0
-pathed /append %mingw%\bin path /user
-pathed /append %mingw%\lib path /user
-pathed /append %mingw%\libexec\gcc\mingw32\%mversion% path /user
-pause
